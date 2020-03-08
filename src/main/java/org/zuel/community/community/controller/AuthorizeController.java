36200a7b2dd0411363409156e918df77182b9102
Henabo <50015277+Henabo@users.noreply.github.com>
2020-03-09 01:15:34 +0800

    commit

--- src/main/java/org/zuel/community/community/controller/AuthorizeController.java
+++ src/main/java/org/zuel/community/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.zuel.community.community.mapper.UserMapper;
 import org.zuel.community.community.model.User;
 import org.zuel.community.community.provider.GithubProvider;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -35,7 +37,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 
@@ -49,13 +52,15 @@ public class AuthorizeController {
         GithubUser githubUser = githubProvider.getuser(accessToken);
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             //登录成功，写cookie和session
+            response.addCookie(new Cookie("token",token));
             request.getSession().setAttribute("user", user);
             return "redirect:/";
 
--- src/main/java/org/zuel/community/community/provider/GithubProvider.java
+++ src/main/java/org/zuel/community/community/provider/GithubProvider.java
@@ -33,7 +33,7 @@ public class GithubProvider {
     public GithubUser getuser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder()
-                .url("https://api.github.com/user?access_token=")
+                .url("https://api.github.com/user")
                 .header("Authorization","token "+accessToken)
                 .build();
         try (Response response = client.newCall(request).execute()) {
