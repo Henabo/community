ee72ba6fec4878d4c660c27dfa0a7fd129889f48
Henabo <50015277+Henabo@users.noreply.github.com>
2020-04-07 13:20:34 +0800

    commit

--- src/test/java/org/zuel/community/community/CommunityApplicationTests.java
+++ src/test/java/org/zuel/community/community/CommunityApplicationTests.java
@@ -3,7 +3,9 @@ package org.zuel.community.community;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
@@ -41,5 +43,12 @@ class CommunityApplicationTests {
             count[num]++;
         }
 
+    }
+
+    @Test
+    void reverseList(){
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
     }
 }
